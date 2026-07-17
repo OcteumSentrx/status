fe1d102bdd37900b3b0d2d667a2e072c1687795d
OcteumSentrx <sxddnsa@gmail.com>
2026-07-17 16:59:29 -0400

    Update +layout.svelte
    Updated footer to include Octeum SentrX powered by Github Status Page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,7 @@
 <footer class="footer footer-center bg-base-300 text-base-content p-4 sticky top-[100vh]">
 	<aside>
 		<p>
-			<a href="https://github.com/Bullrich/GitHub-Status-Page">GitHub-Status-Page</a> made with ❤️
-			by <a href="https://bullrich.dev">Bullrich</a>
+			Status for 👾<a href="https://sentrx.octeum.com">Octeum Sentrx powered by <a href="https://github.com/Bullrich/GitHub-Status-Page">GitHub Status Page</a>
 		</p>
 	</aside>
 </footer>
